25081defa6918b1857a8372fc324a5d3cdb8b4b8
saikrishna321 <saikrishna321@yahoo.com>
2020-07-11 16:57:25 +0530

    3rd comment

--- test/workshop.spec.js
+++ test/workshop.spec.js
@@ -4,14 +4,17 @@ import fetch from 'node-fetch';
 describe('Movie Details', () => {
   it('Should be able to get movie results', async () => {
     const series = await fetch(
-      'http://www.omdbapi.com/?t=Game&Episode=1&apikey=c9079b0f&type=series'
+      'http://www.omdbapi.com/?s=Game&Episode=1&apikey=c9079b0f&type=series'
     );
     const movie = await fetch(
-      'http://www.omdbapi.com/?t=Game&Episode=1&apikey=c9079b0f&type=movie'
+      'http://www.omdbapi.com/?s=Game&Episode=1&apikey=c9079b0f&type=movie'
     );
     const finalSeries = await series.json();
+    const finalMovies = await movie.json();
     expect(series.ok).to.be.true;
     expect(movie.ok).to.be.true;
     expect(finalSeries.Title).to.be.equal('Game of Thrones');
+
+    // TODO Find movies release after 2012
   });
 });
